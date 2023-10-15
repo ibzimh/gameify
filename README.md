0d5be4ae1a0250231fa4b94974c8b5b71782cb6a
Arnav Sampigethaya <arnavsam@gmail.com>
2023-10-15 15:43:52 -0400

    basic express to frontend connection

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# React Native App Readme
+## Welcome to Gameify
 
-## Prerequisites
+### Prerequisites
 
 Before you get started, make sure you have the following software installed:
 
@@ -8,9 +8,9 @@ Before you get started, make sure you have the following software installed:
 
 2. **Xcode (for iOS development):** If you're planning to develop for iOS, you'll need Xcode installed on a macOS machine. You can download it from the Mac App Store. Xcode 15 requires a Mac running macOS Ventura 13.5 or later.
 
-5. **Android Studio (for Android development):** No clue lol
 
-## Getting Started
+
+### Getting Started
 
 Follow these steps to run the React Native app:
 
@@ -19,12 +19,24 @@ Follow these steps to run the React Native app:
 2. **Install Dependencies:** Install the project's dependencies using npm.
 
    ```bash
+   cd src
    npm install
    ```
+
 
 3. **Start the Development Server:**
 
    ```bash
-   cd gameify
    npx expo start
+   ```
+
+4.  **Server dependencies:** 
+   ```bash
+   npm install mongodb express cors dotenv
+   ```
+
+5. **Run Server**
+   ```bash
+   cd ../server
+   node server.js
    ```
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+
+const Categories = () => {
+  const[categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/categories')
+    .then(response=>response.json())
+    .then(data => setCategories(data))
+    .catch(error => console.error(error));
+  }, []);
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={categories}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.item} onPress={() => console.log(item)}>
+            <Text style={styles.title}>{item.category_id}</Text>
+            <Text style={styles.content}>{item.category_name}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.category_id.toString()}
+      />
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center', }, item: { padding: 16, borderBottomWidth: 1, borderBottomColor: '#ccc', }, title: { fontWeight: 'bold', fontSize: 18, marginBottom: 8, }, content: { fontSize: 14, }, });
+
+export default Categories;
