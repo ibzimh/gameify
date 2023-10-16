6378c8c749511ae0417778948f4301f878ca11e6
Julia Epshtein <jepshtein03@gmail.com>
2023-10-16 10:15:28 -0400

    installed nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+
+module.exports = {
+  content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<gameify>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
