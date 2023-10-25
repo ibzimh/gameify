f2df6192859790633f6b491e981e682927b6b7b9
Ibrahim Hasaan <ibzimh@gmail.com>
2023-10-25 11:45:20 -0400

    changing server redirect (working)

--- my-app/env.js
+++ my-app/env.js
@@ -1,5 +1,6 @@
 
-const BACKEND = "http://localhost:8084/oauth2proxy";
+// const BACKEND = "http://localhost:8084/oauth2proxy";
+const BACKEND = "http://localhost:8084/";
 const GOOGLE_CLIENT_ID = '134917274982-c6q8118jneg4ahi6ts9t93l0alevtcuc.apps.googleusercontent.com';
 
 export default {BACKEND, GOOGLE_CLIENT_ID};
