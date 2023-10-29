f764841e4ceb48cc1303cd1fc222683f513b7f48
Ibrahim Hasaan <ibzimh@gmail.com>
2023-10-29 15:05:23 -0400

    removing temporary my-app

--- my-app/env.js
+++
@@ -1,6 +0,0 @@
-
-// const BACKEND = "http://localhost:8084/oauth2proxy";
-const BACKEND = "http://localhost:8084/";
-const GOOGLE_CLIENT_ID = '134917274982-c6q8118jneg4ahi6ts9t93l0alevtcuc.apps.googleusercontent.com';
-
-export default {BACKEND, GOOGLE_CLIENT_ID};
