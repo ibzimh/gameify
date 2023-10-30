1db4d270be7f1a5d31ba4d36d42de4ee8b81d39a
Arnav Sampigethaya <arnavsam@gmail.com>
2023-10-30 17:52:20 -0400

    Alpha locked

--- src/App.js
+++ src/App.js
@@ -19,12 +19,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginView from "./LoginView";
 import HomeScreen from "./home";
 import UsersScreen from "./team";
-
 import { FontAwesome5 } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native'; 
-
-import HomeScreen from './home';
-import UsersScreen from './team';
 import Leaderboard from './leaderboard';
 import GiftScreen from './reward';
 import TaskScreen from './create_task'; 
@@ -64,91 +59,6 @@ const CustomTabScreen = (name, component) => {
   />;
 };
 
-const TaskScreen = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [points, setPoints] = useState('');
-  const [deadline, setDeadline] = useState('');
-
-  const handlePointsButtonPress = (selectedPoints) => {
-    setPoints(selectedPoints);
-  };
-
-  const handlePointsInputChange = (text) => {
-    setPoints(text);
-  };
-
-  const handleDateSelect = (date) => {
-    setDeadline(date.dateString);
-  };
-
-  const handleCreateTask = () => {
-    console.log(`Task Title: ${title}, Description: ${description}, Points: ${points}, Deadline: ${deadline}`);
-  };
-
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>Create a New Task</Text>
-      </View>
-      <Text style={styles.taskDescription}>Task Name</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Task Title"
-        value={title}
-        onChangeText={setTitle}
-      />
-      <View style={styles.pointsContainer}>
-        <Text style={styles.pointsText}>Points:</Text>
-        <ScrollView horizontal>
-          {[100, 200, 300, 400, 500, 600].map((value) => (
-            <TouchableOpacity
-              key={value}
-              style={[
-                styles.pointsButton,
-                points === value.toString() && styles.selectedPointsButton,
-              ]}
-              onPress={() => handlePointsButtonPress(value.toString())}
-            >
-              <Text style={styles.pointsButtonText}>{value}</Text>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </View>
-      <TextInput
-        style={styles.input}
-        placeholder="Points (e.g., 500)"
-        keyboardType="numeric"
-        value={points}
-        onChangeText={handlePointsInputChange}
-      />
-      <TouchableOpacity style={styles.deadlineContainer}>
-        <Text style={styles.deadlineText}>Deadline:</Text>
-        <Text style={styles.selectedDeadline}>{deadline}</Text>
-      </TouchableOpacity>
-      <Calendar
-        onDayPress={handleDateSelect}
-        markedDates={deadline ? {[deadline]: {selected: true, selectedColor: '#007BFF'}} : {}}
-        style={styles.calendar}
-      />
-      <Text style={styles.taskDescription}>Description</Text>
-      <TextInput
-        style={styles.descriptionInput}
-        placeholder="Task Description"
-        value={description}
-        onChangeText={setDescription}
-        multiline
-      />
-      <TouchableOpacity //style = {styles.addButtonContainer}
-        style={styles.addButton}
-        onPress={() => console.log("Add Task Button Pressed")} 
-      >
-        <Text style={styles.addButtonText}>+ Add Task</Text>
-      </TouchableOpacity>
-      
-    </ScrollView>
-  );
-};
 
 const App = () => {
   const [user, setUser] = useState(null);
