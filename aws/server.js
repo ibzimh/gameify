f63f6368947b4a5c223ef8dd3eafb85bf3a4ef57
Ibrahim Hasaan <ibzimh@gmail.com>
2023-11-05 22:39:39 -0500

    removing seperate google url for redirection
    literally no point in having it (excluding the points for having it).

--- aws/server.js
+++ aws/server.js
@@ -22,19 +22,19 @@ const usersRouter = require("./route/choreRoute.js");
 
 app.use("/chores", usersRouter);
 
-app.get('/oauth2proxy/', (req, res) => {
+app.get('/oauth2proxy/*', (req, res) => {
   fs = require('fs');
   res.writeHead(200);
   res.write(fs.readFileSync('page.html'));
   // res.redirect(process.env.EXPO_GO_URL);
 });
 
-app.get('/oauth2proxy/google/*', (req, res) => {
-  fs = require('fs');
-  res.writeHead(200);
-  res.write(fs.readFileSync('page.html'));
-  // res.redirect(process.env.EXPO_GO_URL);
-});
+// app.get('/oauth2proxy/google/*', (req, res) => {
+//   fs = require('fs');
+//   res.writeHead(200);
+//   res.write(fs.readFileSync('page.html'));
+//   // res.redirect(process.env.EXPO_GO_URL);
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
