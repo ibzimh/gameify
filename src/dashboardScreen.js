c77b479617e36518d3fa02358df7e2775639b324
kkolur5 <99568777+kkolur5@users.noreply.github.com>
2023-12-09 21:34:22 -0500

    UI Changes to DashboardScreen

--- src/dashboardScreen.js
+++ src/dashboardScreen.js
@@ -1,264 +1,75 @@
-import React, { useEffect, useState ,createContext, useContext} from 'react';
-import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Modal, TextInput } from 'react-native';
-import { GroupContext } from './team_context'; // Adjust the import path accordingly
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+const window = Dimensions.get('window');
 
-import Config from "./env";
-
-const DashboardScreen = ({user:user, setUser: setUser, setTeams: setTeams}) => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [teamName, setTeamName] = useState('');
-  const [team,setTeam] = useState([]);
-  const {setCurrentGroup } = useContext(GroupContext);
-  useEffect(() => {
-    
-    const fetchTeam = async () =>{
-      try{
-      
-
-    const respond1 = await fetch(Config.BACKEND + "teams");
-    if (!respond1.ok) {
-      throw new Error(`Failed to fetch teams. Status: ${respond1.status}`);
-    }
-
-    const data1 = await respond1.json();
-
-    const teamIds = user.teamIds.map(teamIdObj => teamIdObj.team_id);
-    const currentTeam = data1.data.filter(team => teamIds.includes(team._id));
-
-    setTeam(currentTeam);
-
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-    }
-  };
-
-    fetchTeam();
-  }, []);
-  
-  const handleGroupPress = (group) => {
-    
-    setTeams(true)
-   setCurrentGroup(group)
-
-  };
-  const handleCreateTeamPress = () => {
-    setModalVisible(true);
-  };
-  const handleConfirmCreateTeam = async () => {
-    try {
-
-      const response = await fetch(Config.BACKEND + "teams/add", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ team_name: teamName,usersList: [user._id] }),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to add team');
-      }
-
-     
-      const newTeamData = await response.json();
-      const newTeam = newTeamData.data;
-      // Update the dashboard with the new team
-      setTeam(prevTeam => [...prevTeam, newTeam]);
-      const newGroup = {
-        team_id: newTeam._id,
-        role:"Admin",
-        total_points:0,
-        achievement:null,
-        status:"Active"
-      }
-
-      // Update the teamIds field of the current user
-      const updatedUser = {
-        ...user, // Assuming user holds the current user's data
-        teamIds: [...user.teamIds, newGroup], // Add the new team ID to the user's teamIds
-      };
-  
-      // Update the teamIds field for the current user
-
-      await fetch(Config.BACKEND + `users/${user._id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ teamIds: updatedUser.teamIds }), // Only send the updated teamIds
-      });
-      setUser(updatedUser)
-      // Reset the input field and close the modal
-      setModalVisible(false);
-    } catch (error) {
-      console.error('Error creating team:', error.message);
-    }
-
-  };
+const DashboardScreen = ({ user, setUser, setTeams }) => {
+  const userName = user ? user.name : 'Guest'; // Replace 'name' with the correct property
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>Dashboard</Text>
-      </View>
-      {team.map((group, index) => (
-        <TouchableOpacity
-          key={group._id}
-          style={[styles.groupContainer, { backgroundColor: group.color }]}
-          onPress={() => handleGroupPress(group)}
-        >
-          <Text style={styles.groupName}>{group.team_name}</Text>
-        </TouchableOpacity>
-      ))}
-      
-
-      {/* The Modal is only an overlay and should not replace the existing content */}
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('./img/people.png')} // Replace with the actual path to your image
+        style={styles.backgroundImage}
       >
-        <View style={styles.modalOverlay}>
-          <View style={styles.modalContent}>
-            <TextInput
-              style={styles.input}
-              onChangeText={setTeamName}
-              value={teamName}
-              placeholder="Enter Team Name"
-              placeholderTextColor="#555555"
-            />
-            <TouchableOpacity
-              style={styles.confirmButton}
-              onPress={handleConfirmCreateTeam}
-            >
-              <Text style={styles.confirmButtonText}>Confirm Team Creation</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.cancelButton}
-              onPress={() => setModalVisible(false)}
-            >
-              <Text style={styles.cancelButtonText}>Cancel</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-
-      {/* This button shows the modal when pressed */}
-      <TouchableOpacity
-        style={styles.createButton}
-        onPress={handleCreateTeamPress}
-      >
-        <Text style={styles.createButtonText}>Create Team</Text>
+        {/* Other content goes here */}
+      </ImageBackground>
+      {/* Welcome Text */}
+      <Text style={styles.welcomeText}>Welcome {userName}</Text>
+      {/* View Teams Button */}
+      <TouchableOpacity style={styles.viewTeamsButton} onPress={() => navigation.navigate('TeamsScreen')}>
+        <Text style={styles.buttonText}>View Teams</Text>
       </TouchableOpacity>
-    </ScrollView>
-
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    justifyContent: 'center', // Aligns children vertically in the center
+    alignItems: 'center', // Aligns children horizontally in the center
   },
-  headerContainer: {
-    backgroundColor: '#a593e0',
-    paddingVertical: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#ffffff',
-  },
-  groupContainer: {
-    backgroundColor: '#ffffff', // White background for the cards
-    paddingVertical: 70, // Vertical padding inside the cards
-    paddingHorizontal: 20, // Horizontal padding inside the cards
-    marginVertical: 10, // Margin between the cards
-    marginHorizontal: 16, // Margin on the sides of the cards
-    borderRadius: 10, // Rounded corners
-    borderWidth: 1,
-    borderColor: '#e0e0e0', // Border color
-    flexDirection: 'row', // To align the text and the menu icon
-    alignItems: 'center', // To center the content vertically
-    justifyContent: 'space-between', // To create space between the text and the menu icon
-    shadowColor: '#000', // Shadow color for iOS
-    shadowOffset: { width: 0, height: 1 }, // Shadow position for iOS
-    shadowOpacity: 0.2, // Shadow opacity for iOS
-    shadowRadius: 3, // Shadow blur radius for iOS
-    elevation: 3, // Elevation for Android to create shadow
-  },
-  groupName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  createButton: {
-    backgroundColor: '#007bff',
-    padding: 15,
-    marginHorizontal: 20,
-    marginTop: 10,
-    borderRadius: 10,
-  },
-  createButtonText: {
-    textAlign: 'center',
-    color: '#ffffff',
-    fontSize: 18,
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 35, // Increase padding for bigger content area
-    borderRadius: 10,
+  viewTeamsButton: {
+    position: 'absolute', // Position the button over the container view
+    bottom: 50, // Distance from the bottom of the screen
+    paddingVertical: 25, // Vertical padding for the button
+    backgroundColor: '#000', // Button background color
+    borderRadius: 20, // Rounded corners for the button
+    alignItems: 'center', // Center the text inside the button
+    justifyContent: 'center', // Center the button content
+    elevation: 2, // Shadow for Android
+    // For iOS shadow
+    paddingHorizontal: 25, // Vertical padding for the button
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
-    width: '90%', // Set a width relative to the screen size
-    alignSelf: 'center', // Center the modal on the screen
   },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    width: '100%',
-    // Add this if you also want to change the text color when user types
-    color: '#000000', // This sets the input text color to black
-  },
-  confirmButton: {
-    backgroundColor: '#2196F3',
-    padding: 10,
-    elevation: 2,
-  },
-  confirmButtonText: {
-    color: 'white',
+  welcomeText: {
+    fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center',
+    position: 'absolute', // You can use absolute positioning
+    top: 50, // Adjust the distance from the top
+    alignSelf: 'center', // This centers the text horizontally
+    color: '#fff', // Change the color if needed
   },
-  cancelButton: {
-    backgroundColor: '#ff6347',
-    padding: 10,
-    marginTop: 10,
-    elevation: 2,
+  buttonText: {
+    color: '#fff', // Text color for the button
+    fontSize: 16, // Text font size
+    fontWeight: 'bold', // Font weight for the text
   },
-  cancelButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+  backgroundImage: {
+    position: 'absolute',
+    width: window.width, // Increase width by 200 units
+    height: window.height, // Increase height by 200 units
+    marginHorizontal: -60, // Pull the image outwards by 100 units on both sides
+    marginVertical: -100, // Pull the image outwards by 100 units on top and bottom
+    top: 50, // Align the top edge of the image with the container
+    left: 10, // Align the left edge of the image with the container
   },
-  // ...add any other styles you might need
+  // ... other styles as needed ...
 });
 
 export default DashboardScreen;
